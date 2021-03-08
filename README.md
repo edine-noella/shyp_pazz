572109a74584297acced819cf2215874e39c8016
PatrickNiyogitare28 <patrickniyogitare28@gmail.com>
2021-03-08 21:26:21 +0200

    chore: added configurations #2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # shypment-backend
+Shypment server application
+<br>
+<u>
+<h2>Creating a Modal</h2>
+</u>
+To create a model run a below like command. The case below is to create a TestUser model 
+
+``sequelize model:generate --name TestUser --attributes name:string,email:string``
--- src/database/models/testuser.js
+++ src/database/models/testuser.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class TestUser extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,12 +13,12 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  User.init({
+  TestUser.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'TestUser',
   });
-  return User;
+  return TestUser;
 };
